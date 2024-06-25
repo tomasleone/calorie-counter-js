4f0dadb8ffa7b474e8e5487f0fb6bc95ed8bfcfc
Tomas Leone <tomasnahuelleone@gmail.com>
2024-06-25 15:42:51 -0300

    se agregaron funciones

--- script.js
+++ script.js
@@ -16,8 +16,20 @@ function isInvalidInput(str) {
   return str.match(regex);
 }
 
-
 function addEntry() {
-  const targetId = '#' + entryDropdown.value;
-  const targetInputContainer = document.querySelector(targetId + ' .input-container');
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input
+    type="number"
+    min="0"
+    id="${entryDropdown.value}-${entryNumber}-calories"
+    placeholder="Calories"
+  />`;
+  targetInputContainer.innerHTML += HTMLString;
 }
+
+addEntryButton.addEventListener("click", addEntry);
